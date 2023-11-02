2fdc6209541c87a84ad24fe90a3e3d962327fef0
Katherin Jimenez <katjimenez98@gmail.com>
2023-11-02 16:21:04 -0400

    Adding icons and styling to sidebar

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "./Sidebar.css";
 
 export default function Sidebar() {
@@ -12,7 +13,68 @@ export default function Sidebar() {
 export function SideBarIcons() {
   return (
     <div className="sidebar-icons">
-      <div className="sidebar-logo"></div>
+      <div className="sidebar-logo">
+        <Link to="/">
+          <div className="sidebar-icon">
+            <span className="material-symbols-outlined">run_circle</span>
+            <span className="icon-text"> APP NAME </span>
+          </div>
+        </Link>
+      </div>
+      <div className="top-sidebar">
+        <Link to="/">
+          <div className="sidebar-icon">
+            <span className="material-symbols-outlined">desktop_windows</span>
+            <span className="icon-text"> Dashboard </span>
+          </div>
+        </Link>
+      </div>
+      <div className="middle-sidebar">
+        <Link to="/">
+          <div className="sidebar-icon">
+            <span className="material-symbols-outlined">explore</span>
+            <span className="icon-text"> EXPLORE COACHES</span>
+          </div>
+        </Link>
+        <Link to="/">
+          <div className="sidebar-icon">
+            <span className="material-symbols-outlined">group</span>
+            <span className="icon-text"> MY COACH </span>
+          </div>
+        </Link>
+        <Link to="/">
+          <div className="sidebar-icon">
+            <span className="material-symbols-outlined">edit_note</span>
+            <span className="icon-text"> LOG ACTIVITIES </span>
+          </div>
+        </Link>
+        <Link to="/">
+          <div className="sidebar-icon">
+            <span className="material-symbols-outlined">exercise</span>
+            <span className="icon-text"> WORKOUTS </span>
+          </div>
+        </Link>
+      </div>
+      <div className="bottom-sidebar">
+        <Link to="/">
+          <div className="sidebar-icon">
+            <span className="material-symbols-outlined">account_circle</span>
+            <span className="icon-text"> Profile </span>
+          </div>
+        </Link>
+        <Link to="/">
+          <div className="sidebar-icon">
+            <span className="material-symbols-outlined">settings</span>
+            <span className="icon-text"> Settings </span>
+          </div>
+        </Link>
+        <Link to="/">
+          <div className="sidebar-icon">
+            <span className="material-symbols-outlined">logout</span>
+            <span className="icon-text"> Log Out </span>
+          </div>
+        </Link>
+      </div>
     </div>
   );
 }
