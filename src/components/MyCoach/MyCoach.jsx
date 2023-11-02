701be6ca9faee87b8bb867495d5f39269d157a42
Katherin Jimenez <katjimenez98@gmail.com>
2023-11-02 17:02:06 -0400

    skeleton components for MyCoach and Explore Coaches page

--- src/components/MyCoach/MyCoach.jsx
+++ src/components/MyCoach/MyCoach.jsx
@@ -1,6 +1,6 @@
 import React from "react";
-import "./ExploreCoaches.css";
+import "./MyCoaches.css";
 
 export default function MyCoaches() {
-  return <div className="explore-coaches">Explore Coaches page</div>;
+  return <div className="explore-coaches">My Coaches page</div>;
 }
