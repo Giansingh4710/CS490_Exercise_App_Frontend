4310a952d9f7f484031257efca97e37e655d0bd9
Demetris <perdikos.jimmy@gmail.com>
2023-11-23 01:17:16 -0500

    Added updates to mycoach

--- src/components/MyCoach/MyCoach.jsx
+++ src/components/MyCoach/MyCoach.jsx
@@ -2,14 +2,30 @@ import React from "react";
 import "./MyCoach.css";
 import MyMessagesWithCoach from "./MyMessagesWithCoach/MyMessagesWithCoach";
 import MyAssignedWorkouts from "./MyAssignedWorkouts/MyAssignedWorkouts";
+
 export default function MyCoach() {
-  // to do: add logic to fetch coaches name
-  var coachName = "Doe";
+  // TODO: Add logic to fetch coach's name and details
+  var coachName = "Coach Doe"; // You will replace this with actual data
+
   return (
-    <div className="my-coach">
-      <MyMessagesWithCoach coachName={coachName} />
-      <MyAssignedWorkouts />
-      <p className="fire-coach-link"> Terminate Coach {coachName} </p>
+    <div className="my-coach-container">
+      <header className="my-coach-header">
+        <h1>MY COACH</h1>
+        <div className="coach-details">
+          <h2>{coachName}</h2>
+          {/* Placeholder for the star rating component */}
+          <div className="star-rating">: ★★★★✰ </div>
+        </div>
+      </header>
+
+      <div className="content">
+        <MyAssignedWorkouts />
+        <MyMessagesWithCoach coachName={coachName} />
+      </div>
+
+      <footer>
+        <button className="terminate-coach">Terminate {coachName}</button>
+      </footer>
     </div>
   );
 }
