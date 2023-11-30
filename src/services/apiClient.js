b576670feef9978d7af2b5570e7c3dc5d429de8e
Katherin Jimenez <katjimenez98@gmail.com>
2023-11-30 10:55:04 -0500

    Adding 3 requests to apiClient for explore coaches page

--- src/services/apiClient.js
+++ src/services/apiClient.js
@@ -1,60 +1,84 @@
-import axios from "axios";
-import API_BASE_URL from "../constants";
+import axios from 'axios'
+import API_BASE_URL from '../constants'
 
 class ApiClient {
   constructor(remoteHostUrl) {
-    this.remoteHostUrl = remoteHostUrl;
-    this.token = null;
-    this.tokenName = "fitness_token";
+    this.remoteHostUrl = remoteHostUrl
+    this.token = null
+    this.tokenName = 'fitness_token'
   }
 
   setToken(token) {
-    this.token = token;
-    localStorage.setItem(this.tokenName, token);
+    this.token = token
+    localStorage.setItem(this.tokenName, token)
   }
 
   async request({ endpoint, method = `GET`, data = {} }) {
-    const url = `${this.remoteHostUrl}/${endpoint}`;
+    const url = `${this.remoteHostUrl}/${endpoint}`
 
     const headers = {
-      "Content-Type": "application/json",
-      Authorization: this.token ? `Bearer ${this.token}` : "",
-    };
+      'Content-Type': 'application/json',
+      Authorization: this.token ? `Bearer ${this.token}` : '',
+    }
 
     try {
-      const res = await axios({ url, method, data, headers });
-      return { data: res.data, error: null };
+      const res = await axios({ url, method, data, headers })
+      return { data: res.data, error: null }
     } catch (error) {
-      console.error({ errorResponse: error.response });
-      const message = error?.response?.data?.error;
-      console.log("message:", message);
-      return { data: null, error: message || String(error) };
+      console.error({ errorResponse: error.response })
+      const message = error?.response?.data?.error
+      console.log('message:', message)
+      return { data: null, error: message || String(error) }
     }
   }
 
-  // ----------------------- authentication ----------------------- //
+  // ----------------------- authentication requests ----------------------- //
   async login(credentials) {
     return await this.request({
       endpoint: `login`,
       method: `POST`,
       data: credentials,
-    });
+    })
   }
   async register(credentials) {
     return await this.request({
       endpoint: `register`,
       method: `POST`,
       data: credentials,
-    });
+    })
   }
   async fetchUserFromToken() {
-    return await this.request({ endpoint: `auth/me`, method: `GET` });
+    return await this.request({ endpoint: `auth/me`, method: `GET` })
   }
   logoutUser() {
-    this.setToken(null);
-    localStorage.setItem(this.tokenName, "");
+    this.setToken(null)
+    localStorage.setItem(this.tokenName, '')
+  }
+
+  // ----------------------- coaches requests ----------------------- //
+  async getAllCoaches() {
+    return await this.request({
+      endpoint: `api/coaches`,
+      method: `GET`,
+    })
+  }
+
+  async getAllCoachesBySearchTerm(searchTerm) {
+    return await this.request({
+      endpoint: `api/coaches-search-name?name=${encodeURIComponent(
+        searchTerm,
+      )}`,
+      method: `GET`,
+    })
+  }
+
+  async getCoachByID(coachID) {
+    return await this.request({
+      endpoint: `Coaches/${coachID}`,
+      method: `GET`,
+    })
   }
 }
 
-const apiClient = new ApiClient(API_BASE_URL);
-export default apiClient;
+const apiClient = new ApiClient(API_BASE_URL)
+export default apiClient
