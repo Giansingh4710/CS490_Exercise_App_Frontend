55d6dffee35da5820485cec5fc3e7cb89ad569bf
Rushi <rrpatel@gmail.com>
2023-12-07 17:32:41 -0500

    dfggf

--- src/constants.js
+++ src/constants.js
@@ -1,4 +1,3 @@
-<<<<<<< Updated upstream
 // const PRODUCTION_API_BASE_URL =
   // 'https://cs490-exerciseproj-backend.azurewebsites.net'
 const DEVELOPMENT_API_BASE_URL = 'http://localhost:1313'
@@ -7,13 +6,3 @@ const DEVELOPMENT_API_BASE_URL = 'http://localhost:1313'
 const API_BASE_URL = DEVELOPMENT_API_BASE_URL
 // const API_BASE_URL = PRODUCTION_API_BASE_URL
 export default API_BASE_URL
-=======
-//const PRODUCTION_API_BASE_URL =
-//  "https://cs490-exerciseproj-backend.azurewebsites.net";
-const DEVELOPMENT_API_BASE_URL = "http://localhost:1313";
-
-// change when switching from prod <=> dev
- const API_BASE_URL = DEVELOPMENT_API_BASE_URL;
-//const API_BASE_URL = PRODUCTION_API_BASE_URL;
-export default API_BASE_URL;
->>>>>>> Stashed changes
