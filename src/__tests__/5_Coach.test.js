f99e96cdf3a5733007f716a5e2cdb71696ceba3e
Katherin Jimenez <katjimenez98@gmail.com>
2023-12-20 19:48:58 -0500

    test skeleton

--- src/__tests__/5_Coach.test.js
+++ src/__tests__/5_Coach.test.js
@@ -18,30 +18,6 @@ describe('Coach Tests', function () {
   })
   it('Coach can view list of their clients', async function () {
     await driver.get(baseURL + '/MyClients')
-
-    // const coachListBefore = await driver.findElements(By.css('.coach-card'))
-    // let coachListLengthBefore = coachListBefore.length
-    // await driver.wait(until.elementLocated(By.css('.filter-container')), 10000)
-
-    // const specializationDropdown = await driver.findElement(By.name('specialization'))
-    // const specializationOption = specializationDropdown.findElement(
-    //   By.css('option[value="Train for a sport"]'),
-    // ) // Example value
-    // await specializationOption.click()
-
-    // // Set a maximum price
-    // const priceInput = await driver.findElement(By.name('selectPrice'))
-    // await priceInput.sendKeys('100') // Example value
-
-    // // Select a state
-    // const stateDropdown = await driver.findElement(By.name('state'))
-    // const stateOption = stateDropdown.findElement(By.css('option[value="California"]')) // Example value
-    // await stateOption.click()
-
-    // const coachListAfter = await driver.findElements(By.css('.coach-card'))
-    // let coachListLengthAfter = coachListAfter.length
-
-    // assert(coachListLengthAfter != coachListLengthBefore)
   })
   it('Coach can send messages to Client', async function () {
     await driver.get(baseURL + '/MyClients')
